575314c6f617099f3aa5278cd8b30ee2717c2cbe
Jason Chan <jason27chan@gmail.com>
2018-04-10 23:37:00 +0000

    role requirements for single robot works

--- soccer/gameplay/plays/testing/test_kick_props.py
+++ soccer/gameplay/plays/testing/test_kick_props.py
@@ -6,6 +6,7 @@ import enum
 import main
 import constants
 import robocup
+import role_assignment
 
 
 # this test repeatedly runs the PivotKick behavior aimed at our goal
@@ -21,8 +22,37 @@ class TestKickProps(play.Play):
         kick.target = constants.Field.OurGoalSegment
         kick.aim_params['desperate_timeout'] = 5
         self.add_subbehavior(kick, 'kick', required=False)
+        
+        self.shell_id = None
+
+        for bhvr in self.all_subbehaviors():
+            if bhvr.robot != None:
+                self.shell_id = bhvr.robot.shell_id()
+
+        # print("Hello")
+        # print(self.shell_id)
+        
 
     def execute_running(self):
+        # print(self.all_subbehaviors)
+
+        for bhvr in self.all_subbehaviors():
+            # print(bhvr)
+            if bhvr.robot != None:
+                self.shell_id=bhvr.robot.shell_id()
+
+        # print(self.shell_id)
         kick = self.subbehavior_with_name('kick')
+
         if kick.is_done_running():
             kick.restart()
+
+
+
+    def role_requirements(self):
+        reqs = super().role_requirements()
+        if self.shell_id is not None:
+            for req in role_assignment.iterate_role_requirements_tree_leaves(reqs):
+                req.required_shell_id = self.shell_id if self.shell_id != None else -1
+        # print(req.required_shell_id)
+        return reqs
