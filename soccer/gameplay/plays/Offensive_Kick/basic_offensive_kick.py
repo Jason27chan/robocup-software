b6eb4de71160005f7dc2e2380c9940fa28f2cd43
Jason Chan <jason27chan@gmail.com>
2020-05-03 18:50:23 -0400

    add basic offensive kick implementation attempt1

---
+++ soccer/gameplay/plays/Offensive_Kick/basic_offensive_kick.py
@@ -0,0 +1,26 @@
+import standard_play
+import situational_play_selection
+import main
+import constants
+
+from tactics import coordinated_pass
+
+## 
+# A play that passes to the center in front of the opponent goal
+#
+class BasicOffensiveKick(standard_play.StandardPlay):
+
+    _situationList = [
+        situational_play_selection.SituationalPlaySelector.Situation.OFFENSIVE_KICk
+    ] # yapf: disable
+
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        self.add_transition(behavior.Behavior.State.start,
+                            behavior.Behavior.State.running, lambda: True,
+                            'Start Play Immediately')
+
+        pass_pt = robocup.Point(0, constants.Field.Length * 3/4)
+
+        self.add_subbehavior(coordinated_pass.CoordinatedPass(pass_pt), 'pass')
