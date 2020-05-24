4d6bb30a0761f46730e09baf74e445a3a95058ae
Jason Chan <jason27chan@gmail.com>
2020-05-24 17:20:17 -0400

    copy paste code from #1249

--- soccer/gameplay/plays/Offensive_Kick/cross_or_center.py
+++ soccer/gameplay/plays/Offensive_Kick/cross_or_center.py
@@ -7,22 +7,63 @@ import situational_play_selection
 
 from tactics import coordinated_pass
 
+#Holds a point or a line, with the associted priority ranging from 0 to the number of lines/pts - 1 (0 is highest priority)
+class PointAndPriority:
+	def __init__(self, line_pt, priority):
+		self.line_pt = line_pt
+		self.priority = priority
+
 ## 
 # A play that passes to the center in front of the opponent goal
 #
 class CrossOrCenter(standard_play.StandardPlay):
+    #point with lowest probability will have its probability scaled by MINIMUM_MULTIPLIER
+    MINIMUM_MULTIPLIER = 0.5
 
     _situationList = [
         situational_play_selection.SituationalPlaySelector.Situation.OFFENSIVE_KICK
     ] # yapf: disable
 
     def __init__(self):
-        super().__init__(continuous=True)
+        super().__init__(continuous = True)
+		reflector = 1
+		#Depending on corner's location the points are refelcted
+		if main.ball().pos.x < 0:
+			reflector = -1
 
-        self.add_transition(behavior.Behavior.State.start,
-                            behavior.Behavior.State.running, lambda: True,
-                            'Start Play Immediately')
+		line1 = PointAndPriority(robocup.Line(robocup.Point(constants.Field.Width / -2 * 
+			0.633 * reflector, 7.45), 
+			robocup.Point(constants.Field.Width / -2 * 0.633 * reflector, 8.5)), 0)
+		line2 = PointAndPriority(robocup.Line(robocup.Point(constants.Field.Width / -2 
+			* 0.187 * reflector, 7.11), 
+			robocup.Point(constants.Field.Width / 2 * 0.147 * reflector, 6.2)), 1)
+		line3 = PointAndPriority(robocup.Line(robocup.Point(constants.Field.Width / 2 * 
+			0.8 * reflector, 5.85), 
+			robocup.Point(constants.Field.Width / 2 * 0.33 * reflector, 5.85)), 2)
 
-        pass_pt = robocup.Point(0, constants.Field.Length * 3/4)
+		lines = [line1, line2, line3]
+		best_points = []
 
-        self.add_subbehavior(coordinated_pass.CoordinatedPass(pass_pt), 'pass')
+		for line in lines:
+			best_points.append(PointAndPriority(self.best_point_on_line(line), line.priority))
+
+		multiplier_increment = MINIMUM_MULTIPLIER/(len(best_points) - 1)
+
+		best_point = max(best_points,key = lambda point: 
+				evaluation.passing.eval_pass(main.ball().pos, point.line_pt) 
+				- multiplier_increment * point.priority)
+		
+		self.add_subbehavior(tactics.coordinated_pass.CoordinatedPass(best_point.line_pt), 'passing')
+		
+	#Takes three points on the line (the two ends and the midpoint) and returns one with highest probability
+	def best_point_on_line(self, PointAndPriority):
+		line = PointAndPriority.line_pt
+		point1 = line.get_pt(0)
+		point2 = line.get_pt(1)
+		point3_x = (point1.x + point2.x) * 0.5
+		point3_y = (point1.y + point1.y) * 0.5
+		point3 = robocup.Point(point3_x, point3_y)
+		points = [point1, point2, point3]
+		best_point = max(points,key = lambda point: 
+			evaluation.passing.eval_pass(main.ball().pos, point))
+		return best_point
